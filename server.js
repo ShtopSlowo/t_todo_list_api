dc4cd851e783503ec79d1e34d5a0cc91aeb6d17e
Foegit <foegit@gmail.com>
2018-08-16 13:04:39 +0300

    Connected to db. Add controlers folder

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const db = require('./src/mongoDB');
 const { serverStartMessage } = require('./src/serverUtils');
 const { userRouter, todosRouter } = require('./src/routes/');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+db.connection.on('open', () => {
+  console.info('MongoDB has been connected!');
+});
+
+db.connection.on('error', (err) => {
+  console.info(err);
+});
 
 app.use(morgan('dev'));
 
@@ -16,6 +25,12 @@ app.get('/', (req, res) => {
   res
     .status(200)
     .send('Todo list api works!');
+});
+
+app.use('*', (req, res) => {
+  res
+    .status(404)
+    .send('Not found');
 });
 
 app.listen(PORT, () => serverStartMessage(PORT));
