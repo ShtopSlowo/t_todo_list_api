ddb53f4320b059f702bba8d2c7c6361fa111459c
Foegit <foegit@gmail.com>
2018-08-16 11:52:23 +0300

    Add morgan, add users and todos routes

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const { serverStartMessage } = require('./src/serverUtils');
+const { userRouter, todosRouter } = require('./src/routes/');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use(morgan('dev'));
+
+app.use('/users', userRouter);
+app.use('/todos', todosRouter);
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .send('Todo list api works!');
+});
 
 app.listen(PORT, () => serverStartMessage(PORT));
---
+++ src/routes/index.js
@@ -0,0 +1,2 @@
+exports.userRouter = require('./users');
+exports.todosRouter = require('./todos');
