68c3d2b94d4450266215067449e09a1b550ed207
Foegit <foegit@gmail.com>
2018-08-16 10:52:31 +0300

    Add eslint disable for console

--- src/serverUtils.js
+++ src/serverUtils.js
@@ -1,7 +1,8 @@
 const { CYAN } = require('./constants/consoleColor');
 
 module.exports.serverStartMessage = (PORT) => {
-  console.log(CYAN, `
+  // eslint-disable-next-line
+  console.info(CYAN, `
   Server is running ...
   Check http://localhost:${PORT}
   `);
